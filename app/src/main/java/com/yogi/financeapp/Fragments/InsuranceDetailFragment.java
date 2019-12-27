f8ad2e17fca5fd04815a38d17134e0aaa884d3f8
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2019-12-28 00:34:21 +0530

    Insurance Detail implemented

---
+++ app/src/main/java/com/yogi/financeapp/Fragments/InsuranceDetailFragment.java
@@ -0,0 +1,63 @@
+package com.yogi.financeapp.Fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.yogi.financeapp.R;
+
+public class InsuranceDetailFragment extends Fragment {
+
+    private static final String TAG = "InsuranceDetailFragment";
+
+    private static final String ARGS_IMAGES_ARRAY = "ARGS_IMAGES_ARRAY";
+    private static final String ARGS_DESCRIPTION = "ARGS_DESCRIPTION";
+
+    private int[] images;
+    private String description;
+
+    public static InsuranceDetailFragment newInstance(int[] images, String description) {
+        InsuranceDetailFragment insuranceDetailFragment = new InsuranceDetailFragment();
+        Bundle args = new Bundle();
+        args.putIntArray(ARGS_IMAGES_ARRAY, images);
+        args.putString(ARGS_DESCRIPTION, description);
+        insuranceDetailFragment.setArguments(args);
+        return insuranceDetailFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.insurance_detail, container, false);
+
+        SliderLayout sliderShow = view.findViewById(R.id.slider);
+
+        description = getArguments().getString(ARGS_DESCRIPTION);
+        images = getArguments().getIntArray(ARGS_IMAGES_ARRAY);
+
+        for (int i = 0; i < images.length; i++) {
+            TextSliderView textSliderView = new TextSliderView(getContext());
+            textSliderView.description(description)
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .image(images[i]);
+
+            sliderShow.addSlider(textSliderView);
+        }
+        sliderShow.stopAutoCycle();
+
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/yogi/financeapp/Fragments/ToolsFragment.java
+++ app/src/main/java/com/yogi/financeapp/Fragments/ToolsFragment.java
@@ -4,18 +4,80 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.card.MaterialCardView;
 import com.yogi.financeapp.R;
 
 public class ToolsFragment extends Fragment {
 
+    MaterialCardView carInsuranceMaterialCardView, termInsuranceMaterialCardView,
+            healthInsuranceMaterialCardView, twoWheelerInsuranceMaterialCardView;
+
+    private int[] images;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tools, container, false);
+        View view = inflater.inflate(R.layout.fragment_tools, container, false);
+
+        carInsuranceMaterialCardView = view.findViewById(R.id.car_insurance_list);
+        termInsuranceMaterialCardView = view.findViewById(R.id.term_insurance_list);
+        healthInsuranceMaterialCardView = view.findViewById(R.id.health_insurance_list);
+        twoWheelerInsuranceMaterialCardView = view.findViewById(R.id.two_wheeler_insurance_list);
+
+
+        carInsuranceMaterialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                images = new int[]{R.drawable.car_insurance_one, R.drawable.car_insurance_two, R.drawable.car_insurance_three};
+                Toast.makeText(getContext(), "Car Insurance", Toast.LENGTH_SHORT).show();
+                InsuranceDetailFragment insuranceDetailFragment = InsuranceDetailFragment.newInstance(images, "Car Insurance");
+
+                getFragmentManager().beginTransaction().replace(R.id.frame_container, insuranceDetailFragment).commit();
+            }
+        });
+
+        termInsuranceMaterialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                images = new int[]{R.drawable.term_insurance_one, R.drawable.term_insurance_two};
+
+                InsuranceDetailFragment insuranceDetailFragment = InsuranceDetailFragment.newInstance(images, "Term Insurance");
+                getFragmentManager().beginTransaction().replace(R.id.frame_container, insuranceDetailFragment).commit();
+
+
+                Toast.makeText(getContext(), "Term", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        healthInsuranceMaterialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                images = new int[]{R.drawable.health_insurance_one, R.drawable.health_insurance_two, R.drawable.health_insurance_three, R.drawable.health_insurance_four};
+
+                InsuranceDetailFragment insuranceDetailFragment = InsuranceDetailFragment.newInstance(images, "Health Insurance");
+
+                getFragmentManager().beginTransaction().replace(R.id.frame_container, insuranceDetailFragment).commit();
+                Toast.makeText(getContext(), "Health", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        twoWheelerInsuranceMaterialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                images = new int[]{R.drawable.two_wheeler_insurance_one, R.drawable.two_wheeler_insurance_two};
+                InsuranceDetailFragment insuranceDetailFragment = InsuranceDetailFragment.newInstance(images, "Two-Wheeler Insurance");
+
+                getFragmentManager().beginTransaction().replace(R.id.frame_container, insuranceDetailFragment).commit();
+                Toast.makeText(getContext(), "Two wheeler", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
